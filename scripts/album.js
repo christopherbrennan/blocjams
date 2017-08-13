9ae788d40c9e6565fc8a2c475252cab76086aded
christopherbrennan <chris.ryan.bren@gmail.com>
2017-08-13 17:41:04 -0400

    assignment-11-complete

--- scripts/album.js
+++ scripts/album.js
@@ -30,13 +30,59 @@
      ]
  };
 
+ var albumArtifact = {
+   title: 'Artifact',
+   artist: 'Sound Tribe Sector 9',
+   label: '1320 Records',
+   year: '2005',
+   albumArtUrl: 'assets/images/album_covers/artifact.png',
+   songs: [
+     {title: 'musical story, yes', duration: '0:28'},
+     {title: 'Better Day', duration: '4:49'},
+     {title: 'By The Morning Sun', duration: '3:35'},
+     {title: 'Tokyo', duration: '7:03'},
+     {title: 'ARTiFACT', duration: '2:12'},
+     {title: 'Native End', duration: '4:31'},
+     {title: 'ReEmergence', duration: '5:01'},
+     {title: 'People', duration: '4:19'},
+     {title: 'GLOgli', duration: '5:20'},
+     {title: 'Today', duration: '4:31'},
+     {title: 'Tonight the Ocean Swallowed the Moon', duration: '2:51'},
+     {title: 'Forest Hu', duration: '0:50'},
+     {title: 'Somesing', duration: '6:49'},
+     {title: 'Trinocular', duration: '4:44'},
+     {title: 'Vibyl', duration: '3:00'},
+     {title: '8 & a extra', duration: '1:28'},
+     {title: 'Possibilites', duration: '3:45'},
+     {title: 'Peoples part II', duration: '4:02'},
+     {title: 'first mist over Clear Lake', duration: '0:56'},
+     {title: 'Music, Us', duration: '3:11'},
+     {title: 'Bonus: Tokyo (radio edit)', duration: '4:04'},
+   ]
+ };
+
+
+ var createSongRow = function(songNumber, songName, songLength) {
+     var template =
+        '<tr class="album-view-song-item">'
+      + '  <td class="song-item-number">' + songNumber + '</td>'
+      + '  <td class="song-item-title">' + songName + '</td>'
+      + '  <td class="song-item-duration">' + songLength + '</td>'
+      + '</tr>'
+      ;
+
+      return template;
+ };
+
+ var albumTitle = document.getElementsByClassName('album-view-title')[0];
+ var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+ var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+ var albumImage = document.getElementsByClassName('album-cover-art')[0];
+ var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
  var setCurrentAlbum = function(album) {
      // #1
-     var albumTitle = document.getElementsByClassName('album-view-title')[0];
-     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-     var albumImage = document.getElementsByClassName('album-cover-art')[0];
-     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
 
      // #2
      albumTitle.firstChild.nodeValue = album.title;
@@ -53,6 +99,18 @@
      }
  };
 
+
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
+
+     var albums = [albumPicasso, albumMarconi, albumArtifact];
+     var index = 1;
+
+     albumImage.addEventListener("click", function(event) {
+       setCurrentAlbum(albums[index]);
+       index++;
+       if (index == albums.length) {
+         index = 0;
+       }
+     });
  };
